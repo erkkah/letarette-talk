a16c1f3f995b6a7622509e606b2f6a2a4f25dd0c
Erik Agsjö <erik.agsjo@gmail.com>
2022-05-12 21:31:17 +0200

    dan generation tweaks

--- demo/songs/create_dan_model.py
+++ demo/songs/create_dan_model.py
@@ -2,27 +2,43 @@ import json
 from sys import argv
 import markovify
 
+
 def load(file: str):
     json_file = open(file)
     return json.loads(json_file.read())
+
 
 def main():
     file = argv[1]
     print(f"Loading from {file}\n")
 
-    state_size = 2
+    title_state_size = 1
+    text_state_size = 2
 
-    title_model = markovify.NewlineText("Brooklyn", state_size=state_size)
-    text_model = markovify.NewlineText("Knows the charmer under me", state_size=state_size)
+    title_model = markovify.NewlineText("Brooklyn", state_size=title_state_size)
+    text_model = markovify.NewlineText(
+        "Ows the charmer under me", state_size=text_state_size
+    )
 
     for song in load(file):
         try:
             title = song["title"]
-            model = markovify.NewlineText(title, retain_original=False, well_formed=False, state_size=state_size)
+            lyrics = song["lyrics"]
+
+            model = markovify.NewlineText(
+                title + " " + lyrics,
+                retain_original=False,
+                well_formed=False,
+                state_size=title_state_size,
+            )
             title_model = markovify.combine([model, title_model])
 
-            lyrics = song["lyrics"]
-            model = markovify.NewlineText(lyrics, retain_original=False, well_formed=False, state_size=state_size)
+            model = markovify.NewlineText(
+                lyrics,
+                retain_original=False,
+                well_formed=False,
+                state_size=text_state_size,
+            )
             text_model = markovify.combine([model, text_model])
         except Exception as ex:
             print(f"Skipping song '{(song['title'])}': {ex}")
@@ -36,5 +52,6 @@ def main():
     texts = open("texts.json", "w")
     texts.write(text_model.to_json())
     texts.close()
+
 
 main()
--- demo/songs/generate_dan.py
+++ demo/songs/generate_dan.py
@@ -1,67 +1,145 @@
-from os import read
-from random import randint
+from time import sleep
+from random import randint, sample, shuffle
 from sys import argv
 import markovify
 import sqlite3
 
+
 def main():
     text_model = load_model("texts.json")
-    title_model = markovify.combine([load_model("titles.json"), text_model], [1, 3])
+    title_model = load_model("titles.json")
+    #title_model = markovify.combine([load_model("titles.json"), text_model], [1, 4])
 
-    db = sqlite3.connect("songs.db")
+    db = sqlite3.connect("songs.db", isolation_level=None)
+    db.execute("PRAGMA journal_mode=WAL")
     setup_tables(db)
 
     num_issues = 100
     if len(argv) > 1:
         num_issues = int(argv[1])
 
-    for _ in range(num_issues):
-        [title, text] = create_song(text_model, title_model)
-        print(f"** {title} **\n{text}")
-        add_song(db, title, text)
+    interval = 0
+    if len(argv) > 2:
+        interval = int(argv[2])
+
+    print(
+        f"Generating {num_issues} songs"
+        + (f" every {interval} seconds" if interval > 0 else "")
+    )
+
+    while True:
+        for _ in range(num_issues):
+            [title, text] = create_song(text_model, title_model)
+            if not title:
+                continue
+            title = make_unique_title(title, db)
+            if interval < 0:
+                print(f"** {title} **\n{text}")
+            else:
+                add_song(db, title, text)
+        if interval <= 0:
+            break
+        else:
+            sleep(interval)
 
     db.close()
 
+
+def tweak_title(title: str) -> str:
+    suffixes = [
+        "alt. take",
+        "live version",
+        "with Harmonica",
+        "reprise",
+        "remix",
+        "demo",
+        "alt. lyrics",
+    ]
+    suffix = sample(suffixes, 1)[0]
+    return f"{title} - {suffix}"
+
+
+def make_unique_title(title: str, db: sqlite3.Connection) -> str:
+    tweak_index = 0
+    tweaked = title
+    while not is_unique_title(tweaked, db):
+        tweaked = tweak_title(title)
+        if tweak_index > 0:
+            tweaked = f"{tweaked} {tweak_index}"
+        tweak_index += 1
+    return tweaked
+
+
+def is_unique_title(title: str, db: sqlite3.Connection) -> bool:
+    cursor = db.execute(
+        """
+        select count(*) from songs
+        where title = ?
+        """,
+        [title],
+    )
+    res = cursor.fetchone()[0]
+    return res == 0
+
+
 def setup_tables(db: sqlite3.Connection):
-    db.execute("""
-    create table if not exists songs (
-        id integer primary key autoincrement,
-        title text,
-        text text,
-        updated timestamp default current_timestamp
-    );
-    """)
+    db.execute(
+        """
+        create table if not exists songs (
+            id integer primary key autoincrement,
+            title text,
+            text text,
+            updated timestamp default current_timestamp
+        );
+        """
+    )
 
-    db.execute("""
-    create trigger if not exists songs_au after update of title, text
-    on songs begin
-        update songs set updated = current_timestamp;
-    end;
-    """)
+    db.execute(
+        """
+        create trigger if not exists songs_au after update of title, text
+        on songs begin
+            update songs set updated = current_timestamp;
+        end;
+        """
+    )
 
-    db.execute("""
+    db.execute(
+        """
     create index if not exists songs_id_updated
         on songs(id, updated)
-    """)
+    """
+    )
+
+    db.execute(
+        """
+        create index if not exists songs_title
+        on songs(title)
+        """
+    )
+
 
 def add_song(db: sqlite3.Connection, title, text):
     db.execute("insert into songs(title, text) values(?, ?)", [title, text])
     db.commit()
+
 
 def create_song(text_model, title_model):
     title = title_model.make_short_sentence(32)
     text = make_text(text_model)
     return [title, text]
 
+
 def make_text(model):
     paragraphs = randint(4, 8)
     text = "\n\n".join([make_paragraph(model) for _ in range(paragraphs)])
     return text
 
+
 def make_paragraph(model):
     sentences = randint(4, 6)
     paragraph = "\n".join([model.make_short_sentence(64) for _ in range(sentences)])
     return paragraph
+
 
 def load_model(file: str):
     model_file = open(file)
@@ -69,5 +147,6 @@ def load_model(file: str):
     model = markovify.Text.from_json(model_data)
     model_file.close()
     return model
+
 
 main()
